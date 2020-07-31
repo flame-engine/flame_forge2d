a5b8d73c50b48bb36f0b705bae4420aea88c58ca
Lukas Klingsbo <lukas.klingsbo@klarna.com>
2020-07-31 19:10:11 +0200

    Point dependencies to flame package

--- lib/sprite_body_component.dart
+++ lib/sprite_body_component.dart
@@ -1,8 +1,9 @@
 import 'dart:ui';
 
-import '../anchor.dart';
-import '../components/component.dart';
-import '../sprite.dart';
+import 'package:flame/anchor.dart';
+import 'package:flame/components/component.dart';
+import 'package:flame/sprite.dart';
+
 import 'body_component.dart';
 import 'box2d_game.dart';
 
