cca445dede2e951e8be49c25fab04437720eec3f
Lukas Klingsbo (spydon) <lukas.klingsbo@gmail.com>
2020-10-28 20:41:29 +0100

    Use local position instead of global

--- example/lib/blob_sample.dart
+++ example/lib/blob_sample.dart
@@ -116,7 +116,7 @@ class BlobSample extends Forge2DGame with TapDetector {
   void onTapDown(TapDownDetails details) {
     super.onTapDown(details);
     final Vector2 screenPosition =
-        Vector2(details.globalPosition.dx, details.globalPosition.dy);
+        Vector2(details.localPosition.dx, details.localPosition.dy);
     final Vector2 worldPosition = viewport.getScreenToWorld(screenPosition);
     add(FallingBox(this, worldPosition));
   }
